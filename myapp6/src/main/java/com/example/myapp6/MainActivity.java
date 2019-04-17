95e252c2286eef30000e686c90d5bdd9becb221c
정민우 <jmw93@naver.com>
2019-04-17 22:24:39 +0900

    .

--- myapp6/src/main/java/com/example/myapp6/MainActivity.java
+++ myapp6/src/main/java/com/example/myapp6/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapp6;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(layoutManager);
+//      어댑터객체 생성 후데이터 어댑터에 입력
+        adapter adapter = new adapter();
+        adapter.addItem(new lating("jmw93","내용이 참 재미있었고 유익했어요"));
+        adapter.addItem(new lating("jmw931012","내용이 참  유익했어요"));
+        adapter.addItem(new lating("jmw931012","내용이 참  유익했어요"));
+        adapter.addItem(new lating("jmw931012","내용이 참  유익했어요"));
+        adapter.addItem(new lating("jmw931012","내용이 참  유익했어요"));
+        adapter.addItem(new lating("jmw931012","내용이 참  유익했어요"));
+        adapter.addItem(new lating("jmw931012","내용이 참  유익했어요"));
+        adapter.addItem(new lating("jmw931012","내용이 참  유익했어요"));
+        adapter.addItem(new lating("jmw931012","내용이 참  유익했어요"));
+
+        recyclerView.setAdapter(adapter);
     }
 }
