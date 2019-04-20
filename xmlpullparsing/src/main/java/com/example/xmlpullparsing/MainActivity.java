38d67524491f0092596606d672b7e56e911d229b
정민우 <jmw93@naver.com>
2019-04-21 01:37:46 +0900

    .

--- xmlpullparsing/src/main/java/com/example/xmlpullparsing/MainActivity.java
+++ xmlpullparsing/src/main/java/com/example/xmlpullparsing/MainActivity.java
@@ -1,96 +1,130 @@
 package com.example.xmlpullparsing;
 
 import android.app.Activity;
-import android.content.res.AssetManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
+import android.widget.TextView;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.InputStream;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class MainActivity extends Activity {
-
-
-
+    TextView textView;
+    String data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ListView listView = findViewById(R.id.listView);
-        ArrayList<Student> list = xmlParsing();
-        Log.d("parser","list모두 생성 완료/파싱완료!!!!!!!");
-        String[] data = new String[list.size()];
+        textView = findViewById(R.id.textView);
 
-        for(int i = 0; i < list.size(); i++){
-            data[i] = list.get(i).getName() +" "
-                    + list.get(i).getAge() + " "
-                    + list.get(i).getAddress();
-        }
+         // 파싱후 내용이담긴 (String) buffer 객체
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                data = XMLparser();
 
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
-        listView.setAdapter(adapter);
-    }
-
-    private ArrayList<Student> xmlParsing() {
-        ArrayList<Student> allList = new ArrayList<Student>();
-
-        try {
-
-            AssetManager am = getResources().getAssets();
-            InputStream is =null;
-
-            is=am.open("file.xml");
-            XmlPullParserFactory parserFactory = XmlPullParserFactory.newInstance();
-            XmlPullParser parser = parserFactory.newPullParser();
-            parser.setInput(is,"UTF-8");
-            Log.d("parser","데이터읽기 성공");
-
-            int eventType = parser.getEventType();
-            Student student = null;
-
-            while (eventType != XmlPullParser.END_DOCUMENT){
-                switch (eventType) {
-                    case XmlPullParser.START_TAG:
-                        String startTag = parser.getName();
-
-                        if(startTag.equals("student")){
-                            student = new Student();
-                            Log.d("parser","파서 첫 객체생성");
-                        }
-                        if (startTag.equals("name")){
-                            student.setName(parser.nextText());
-                        }
-                        if (startTag.equals("age")){
-                            student.setAge(parser.nextText());
-                        }
-                        if (startTag.equals("address")){
-                            student.setAddress(parser.nextText());
-                        }
-                        break;
-                    case XmlPullParser.END_TAG:
-                        String endTag = parser.getName();
-                        if(endTag.equals("student")){
-                            allList.add(student);
-                        }
-                        break;
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    textView.setText(data);
                 }
-
-                // 다음 엘리먼트 읽기
-                eventType = parser.next();
+            });
             }
 
-        } catch (Exception e) {
-            Log.e("MyTag", e.toString());
-        }
-        return allList;
+
+        }).start();
     }
 
+        public String XMLparser(){
+        StringBuffer buffer = new StringBuffer();
+        String Urlrequest ="http://open.ev.or.kr:8080/openapi/services/rest/EvChargerService?serviceKey=" +
+                        "RjzMYQORqJIq4l9YZkCCmV5mTIec%2BdJYC%2BUzK3c2Aogy4I2Y0tZnRI4292OO56Qqr%2FIMajYNHjo5M8Ayz4R05g%3D%3D";
 
-}
+            try {
+                URL url = new URL(Urlrequest);
+                InputStream is = url.openStream();
+                XmlPullParserFactory parserFactory = XmlPullParserFactory.newInstance();
+                XmlPullParser xpp = parserFactory.newPullParser();
+                xpp.setInput(new InputStreamReader(is,"UTF-8"));
+
+
+                int eventType = xpp.getEventType();
+
+                while (eventType != XmlPullParser.END_DOCUMENT){
+                    switch (eventType) {
+                        case XmlPullParser.START_TAG:
+                            String startTag = xpp.getName();
+                            Log.d("minwoo",startTag);
+                            if(startTag.equals("item")){
+                                buffer.append(startTag+"{");
+                                buffer.append("\n");
+                            }
+
+                            if(startTag.equals("statNm")){
+                                xpp.next();
+                                buffer.append(startTag+":"+xpp.getText());
+                                buffer.append("\n");
+                            }
+                            if(startTag.equals("chgerId")){
+                                xpp.next();
+                                buffer.append(startTag+":"+xpp.getText());
+                                buffer.append("\n");
+                            }
+                            if(startTag.equals("chgerType")){
+                                xpp.next();
+                                buffer.append(startTag+":"+xpp.getText());
+                                buffer.append("\n");
+                            }
+                            if(startTag.equals("stat")){
+                                xpp.next();
+                                buffer.append(startTag+":"+xpp.getText());
+                                buffer.append("\n");
+                            }
+                            if(startTag.equals("addrDoro")){
+                                xpp.next();
+                                buffer.append(startTag+":"+xpp.getText());
+                                buffer.append("\n");
+                            }
+                            if(startTag.equals("useTime")){
+                                xpp.next();
+                                buffer.append(startTag+":"+xpp.getText());
+                                buffer.append("\n");
+                            }
+
+
+                            break;
+
+                     /*
+                        case XmlPullParser.TEXT:
+                            String textTag = xpp.getText();
+                                if(textTag != null)
+                                Log.d("minwoo",textTag);*/
+
+                        case XmlPullParser.END_TAG:
+                         String endTag =xpp.getName();
+                         if(endTag.equals("item")){
+                             buffer.append("}");
+                             buffer.append("\n");
+
+                         }
+                    break;
+                    }
+
+                    // 다음 엘리먼트 읽기
+                    eventType = xpp.next();
+                }
+
+            } catch (Exception e) {
+                Log.e("minwoo", e.toString());
+            }
+            return buffer.toString();
+        }
+
+
+    }
+
 
